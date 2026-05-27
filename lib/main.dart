a8e1f603ef6ee1a3c7d72890c5ec13bb63c6f0cb
Luci <lapl1412.work@gmail.com>
2026-05-27 13:31:02 +0700

    feat: integrate Firebase Core and Crashlytics into the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 import 'core/di/injection.dart';
 import 'core/notifications/notifications_service.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await configureDependencies();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await getIt<NotificationsService>().init();
   runApp(const App());
 }
