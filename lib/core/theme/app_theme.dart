4503d5a172ea5f8b948efdd51e4f3c046bd820d4
Luci <lapl1412.work@gmail.com>
2026-05-27 09:09:50 +0700

    feat: add google_fonts package and integrate Inter font into light and dark themes

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   const AppTheme._();
 
-  static ThemeData light() => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      );
+  static ThemeData light() {
+    final base = ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        brightness: Brightness.light,
+      ),
+      useMaterial3: true,
+    );
+    return base.copyWith(textTheme: _textTheme(base.textTheme));
+  }
 
-  static ThemeData dark() => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      );
+  static ThemeData dark() {
+    final base = ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        brightness: Brightness.dark,
+      ),
+      useMaterial3: true,
+    );
+    return base.copyWith(textTheme: _textTheme(base.textTheme));
+  }
+
+  static TextTheme _textTheme(TextTheme base) =>
+      GoogleFonts.interTextTheme(base);
 }
